069ef31d80701c6c36028e5077a4b1c32b0c993e
gaau511 <gaau511@gmail.com>
2025-05-17 16:44:05 +0900

    feat(domain): Create ChatRoom Entity

---
+++ src/main/java/dev/gaau/messenger/domain/ChatRoom.java
@@ -0,0 +1,41 @@
+package dev.gaau.messenger.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@Table(name = "chat_room")
+@NoArgsConstructor
+public class ChatRoom {
+
+    @Builder
+    public ChatRoom(String title) {
+        this.title = title;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "chatRoom")
+    private List<Message> messages = new ArrayList<>();
+
+    @OneToMany(mappedBy = "chatRoom")
+    private List<MemberChatRoom> memberChatRooms = new ArrayList<>();
+}
