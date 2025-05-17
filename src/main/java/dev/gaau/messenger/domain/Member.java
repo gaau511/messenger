6ed5b86597f76869672820939ff3d7ec70169fc3
gaau511 <gaau511@gmail.com>
2025-05-17 16:44:19 +0900

    feat(domain): Create Member Entity

---
+++ src/main/java/dev/gaau/messenger/domain/Member.java
@@ -0,0 +1,63 @@
+package dev.gaau.messenger.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "member")
+@Getter
+@NoArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "birth", nullable = false)
+    private LocalDate birth;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberChatRoom> memberChatRooms = new ArrayList<>();
+
+    @Builder
+    public Member(String username, String password, String name, String gender, String email, String nickname, LocalDate birth) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.gender = gender;
+        this.email = email;
+        this.nickname = nickname;
+        this.birth = birth;
+    }
+
+}
