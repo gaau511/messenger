be780509c1e384c39f93c58ccb155487f0a2fa30
gaau511 <gaau511@gmail.com>
2025-05-17 16:44:51 +0900

    feat(domain): Create MemberChatRoom Entity
    - This is a junction table between Member and Chatroom entities.

---
+++ src/main/java/dev/gaau/messenger/domain/MemberChatRoom.java
@@ -0,0 +1,29 @@
+package dev.gaau.messenger.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "member_chat_room")
+@NoArgsConstructor
+public class MemberChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+
+    @Builder
+    public MemberChatRoom(Member member, ChatRoom chatRoom) {
+        this.member = member;
+        this.chatRoom = chatRoom;
+    }
+}
