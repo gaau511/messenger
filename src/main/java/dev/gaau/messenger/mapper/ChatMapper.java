bd47dd3dee5709c210a8ec5cc028d57f49095096
gaau511 <gaau511@gmail.com>
2025-05-20 00:13:44 +0900

    feat(mapper): Add message related conversion methods

--- src/main/java/dev/gaau/messenger/mapper/ChatMapper.java
+++ src/main/java/dev/gaau/messenger/mapper/ChatMapper.java
@@ -1,7 +1,11 @@
 package dev.gaau.messenger.mapper;
 
 import dev.gaau.messenger.domain.ChatRoom;
+import dev.gaau.messenger.domain.Member;
+import dev.gaau.messenger.domain.Message;
+import dev.gaau.messenger.dto.request.MessageRequestDto;
 import dev.gaau.messenger.dto.response.ChatRoomDto;
+import dev.gaau.messenger.dto.response.MessageDto;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +16,29 @@ public class ChatMapper {
                 .id(chatRoom.getId())
                 .createdAt(chatRoom.getCreatedAt())
                 .title(chatRoom.getTitle())
+                .build();
+    }
+
+    public Message toMessage(MessageRequestDto messageRequestDto, ChatRoom chatRoom,
+                             Member member) {
+        return Message.builder()
+                .type(messageRequestDto.getType())
+                .contents(messageRequestDto.getContents())
+                .chatRoom(chatRoom)
+                .member(member)
+                .readCount(chatRoom.getMemberChatRooms().size())
+                .build();
+    }
+
+    public MessageDto messageToMessageDto(Message message) {
+        return MessageDto.builder()
+                .id(message.getId())
+                .type(message.getType())
+                .contents(message.getContents())
+                .createdAt(message.getCreatedAt())
+                .chatRoomId(message.getChatRoom().getId())
+                .memberId(message.getMember().getId())
+                .readCount(message.getChatRoom().getMemberChatRooms().size())
                 .build();
     }
 }
