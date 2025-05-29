7494dbc8da8053f03f909b2b4f680c03626b1b73
gaau511 <gaau511@gmail.com>
2025-05-30 02:05:01 +0900

    feat(controller): Create method and page for chat room creation

---
+++ src/main/java/dev/gaau/messenger/controller/ChatController.java
@@ -0,0 +1,36 @@
+package dev.gaau.messenger.controller;
+
+import dev.gaau.messenger.dto.request.ChatRoomCreateRequestDto;
+import dev.gaau.messenger.dto.request.MessageRequestDto;
+import dev.gaau.messenger.dto.response.ChatRoomDto;
+import dev.gaau.messenger.dto.response.ChatRoomSummaryDto;
+import dev.gaau.messenger.dto.response.MessageDto;
+import dev.gaau.messenger.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/view/chat")
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final ChatService chatService;
+    private final Long temporaryMemberId = 1L;
+
+    @GetMapping("/create")
+    public String createChatRoomPage (Model model) {
+        model.addAttribute("members", chatService.getMembers());
+        return "chat/create";
+    }
+
+    @PostMapping("/create")
+    public String createChatRoom(@ModelAttribute ChatRoomCreateRequestDto chatRoomCreateRequestDto) {
+        ChatRoomDto chatRoom = chatService.createChatRoom(temporaryMemberId, chatRoomCreateRequestDto);
+        return "redirect:room/" + chatRoom.getId();
+    }
+
+}
