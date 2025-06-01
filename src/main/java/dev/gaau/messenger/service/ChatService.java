9f2b1282db9f52d8837394ce9c2fef71de7511db
gaau511 <gaau511@gmail.com>
2025-06-02 04:06:16 +0900

    refactor(chatroom): Use convenience methods for managing associations when creating chatroom

--- src/main/java/dev/gaau/messenger/service/ChatService.java
+++ src/main/java/dev/gaau/messenger/service/ChatService.java
@@ -44,47 +44,35 @@ public class ChatService {
         // 2. 요청 DTO로부터 참여자 ID 목록 추출
         List<Long> participantsIds = chatRoomCreateRequestDto.getMemberIds();
 
+        // 3. 참여자들을 담을 리스트 생성
         Set<Member> participants = new HashSet<>();
 
-        // 4. 각 참여자 ID에 대해 Member 엔티티를 조회하여 participants 리스트에 추가
+        // 4. 로그인한 사용자를 참여자 목록에 추가
+        participants.add(loggedInMember);
+
+        // 5. 각 참여자 ID에 대해 Member 엔티티를 조회하여 participants 리스트에 추가
         participantsIds.forEach(memberId -> {
             Member member = memberRepository.findById(memberId)
                     .orElseThrow(() -> new RuntimeException("Cannot find member with id: " + memberId));
             participants.add(member);
         });
 
-        // 5. 로그인한 사용자도 참여자 목록에 추가
-        participants.add(loggedInMember);
-
         // 6. 채팅방 이름 생성
-        String title = chatRoomCreateRequestDto.getTitle() == null ? makeDefaultChatRoomTitle(participants) : chatRoomCreateRequestDto.getTitle();
+        String title = chatRoomCreateRequestDto.getTitle().isEmpty() ? makeDefaultChatRoomTitle(participants) : chatRoomCreateRequestDto.getTitle() ;
 
-        // 6. ChatRoom 엔티티 생성
+        // 7. ChatRoom 엔티티 생성
         ChatRoom chatRoom = ChatRoom.builder()
                 .title(title)
                 .build();
 
-        // 7. ChatRoom 저장
-        ChatRoom savedChatRoom = chatRoomRepository.save(chatRoom);
 
         // 8. 각 참여자에 대해 MemberChatRoom 엔티티 생성 및 저장
-        participants.forEach(member -> {
+        participants.forEach(member -> member.addChatRoom(chatRoom));
 
-            // 8-1. MemberChatRoom 엔티티 생성 (참여자와 채팅방 관계 매핑)
-            MemberChatRoom memberChatRoom = MemberChatRoom.builder()
-                    .chatRoom(savedChatRoom)
-                    .member(member)
-                    .build();
+        // 9. ChatRoom 저장 시, Cascade option에 따라 memberChatRoom도 함께 영속
+        ChatRoom savedChatRoom = chatRoomRepository.save(chatRoom);
 
-            // 8-2. MemberChatRoom 저장
-            MemberChatRoom savedMemberChatRoom = memberChatRoomRepository.save(memberChatRoom);
-
-            // 8-3. 양방향 연관 관계 설정
-            savedChatRoom.getMemberChatRooms().add(savedMemberChatRoom);
-            member.getMemberChatRooms().add(savedMemberChatRoom);
-        });
-
-        // 9. ChatRoom 엔티티를 DTO로 변환하여 반환
+        // 10. ChatRoom 엔티티를 DTO로 변환하여 반환
         return chatMapper.chatRoomToChatRoomDto(savedChatRoom);
     }
 
